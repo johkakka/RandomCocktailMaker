da9735d361f9e264cb5a31474dda5dc8cecd89f7
johkakka <johkakka.rits@gmail.com>
2023-05-06 14:41:00 +0900

    Initial commit

---
+++ src/main/java/dev/johkakka/randomcocktailmaker/RandomCocktailMakerApplication.java
@@ -0,0 +1,13 @@
+package dev.johkakka.randomcocktailmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCocktailMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomCocktailMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/johkakka/randomcocktailmaker/RandomCocktailMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johkakka.randomcocktailmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCocktailMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
