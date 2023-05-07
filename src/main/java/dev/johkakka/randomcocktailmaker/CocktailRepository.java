89d0d9f0b46e453bb1c3cc92048b022adb4111c6
johkakka <johkakka.rits@gmail.com>
2023-05-07 10:26:51 +0900

    [fix] 二重にアノテーションされている問題を解決

--- src/main/java/dev/johkakka/randomcocktailmaker/CocktailRepository.java
+++ src/main/java/dev/johkakka/randomcocktailmaker/CocktailRepository.java
@@ -1,8 +1,5 @@
 package dev.johkakka.randomcocktailmaker;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface CocktailRepository {
     public String get(int i);
 }
--- src/main/java/dev/johkakka/randomcocktailmaker/CocktailService.java
+++ src/main/java/dev/johkakka/randomcocktailmaker/CocktailService.java
@@ -1,9 +1,7 @@
 package dev.johkakka.randomcocktailmaker;
 
 import dev.johkakka.randomcocktailmaker.domain.Cocktail;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CocktailService {
     public Cocktail get();
 }
